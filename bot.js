8942b161fa2ebcd7cfb5899ea6e07c7634f16c0c
Snyoraby <91962941+Snyoraby@users.noreply.github.com>
2021-10-05 17:05:00 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,7 +18,7 @@ client.on('message', message => {
 if (message.content === prefix+'fast') {
       let count = 0;
       let ecount = 0;
-      for(let x = 0; x < 30000; x++) {
+      for(let x = 0; x < 3; x++) {
         message.channel.send(`**??? ???? ??? **[ " ${x} " ]`)
           .then(m => {
             count++;
