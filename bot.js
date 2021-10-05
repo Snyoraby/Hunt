d4cf8759d172932e7626c498bc215078ab24d2b3
Snyoraby <91962941+Snyoraby@users.noreply.github.com>
2021-10-05 18:06:34 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,9 +14,9 @@ client.login(process.env.TOKEN);
 
 client.on('ready', async() => {
 var server = "894597075285712957"; 
-var channel = " 894973008282796053 ";
+var channel = "894973008282796053";
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('s1 $hunt gods ')
+    client.guilds.get(server).channels.get(channel).send('s1 $hunt gods')
     },45000);
 });
           
