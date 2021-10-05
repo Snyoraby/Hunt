24033ab64115ce44476eb107b685302cd49ed72c
Snyoraby <91962941+Snyoraby@users.noreply.github.com>
2021-10-05 16:41:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -68,13 +68,9 @@ client6.on('message', message => {
 
 client.on('message', message => {
 if (message.content === prefix+'hunt') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 100; x++) {
-        message.channel.send(` s1 $hunt gods `)
-          .then(m => {
-            count++;
-            12000;
+      setInterval(() => {
+ message.channel.send(`hy`).then(() => count++);
+}, 10000);
           })
           
         }
